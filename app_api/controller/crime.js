ce7a7ab1dea9948caa61b9ed8498d218f8f6b424
Mzala123 <justicemwanzamj@gmail.com>
2022-03-21 14:32:27 +0200

    Adding crime continuation API

--- app_api/controller/crime.js
+++ app_api/controller/crime.js
@@ -1,6 +1,8 @@
 const { request } = require("express")
+const { send } = require("express/lib/response")
 var mongoose = require("mongoose")
 var Suspect = mongoose.model("Suspect")
+var User = mongoose.model("User")
 
 var sendJSONresponse = function(res, status, content){
     res.status(status)
@@ -9,10 +11,30 @@ var sendJSONresponse = function(res, status, content){
 
 
 module.exports.crimesCreate = function(req, res){
-    
+     
 }
 
-var addCrime = function(req, res, suspect){
+var getRegisteringOfficer = function(req, res, callback){
+     if(req.payload && req.payload.email){
+         User
+           .findOne({email : req.payload.email})
+           .exec(function(err, user){
+               if(!user){
+                   sendJSONresponse(res, 404, {"message":"user not found"})
+                   return
+               }else if(err){
+                   console.log(err)
+                   sendJSONresponse(res, 404, err)
+                   return
+               }
+               callback(req, res, user.name)
+           })
+     }else{
+         sendJSONresponse(res, 404, {"message":"user not found"})
+     }
+}
+
+var addCrime = function(req, res, officer){
     if(!suspect){
         sendJSONresponse(res, 404, {"message":"suspect not found"})
     }else{
@@ -21,7 +43,7 @@ var addCrime = function(req, res, suspect){
             counts: req.body.counts,
             offenseDate: req.body.offenseDate,
             offenseDescription: req.body.offenseDescription,
-            registeringOfficer: req.body.registeringOfficer,
+            registeringOfficer: officer,
         })
         suspect.save(function(err, suspect){
             var thisCrime
